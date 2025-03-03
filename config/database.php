3fdbc77b0685b565d1dd36f0a9c8d44566275d4b
Gustavo Barragan <dev@droni.co>
2025-03-02 22:53:27 -0500

    sqlsrv config

--- config/database.php
+++ config/database.php
@@ -103,8 +103,6 @@ return [
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
